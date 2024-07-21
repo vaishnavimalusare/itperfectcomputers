b07469a4974b7d6c9db143baca6894821ff375be
vaishnavimalusare <vaishvavi1982@gmail.com>
2024-07-21 21:14:27 +0530

    Add files via upload

---
+++ process.php
@@ -0,0 +1,9 @@
+<?php
+// process.php
+// Do some processing, for example, get data from a database or perform some calculations
+
+// For this example, we just return a simple message
+echo "Hello, this is the response from the PHP script!";
+
+
+?>
